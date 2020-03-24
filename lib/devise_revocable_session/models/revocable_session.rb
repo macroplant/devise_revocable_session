749301d06ed21f84eb4859becbc9cc49bcaa6b2d
Carl Allen <carl.allen@dispatchwithus.com>
2020-03-24 12:55:35 -0500

    wrap all writes in role

--- lib/devise_revocable_session/models/revocable_session.rb
+++ lib/devise_revocable_session/models/revocable_session.rb
@@ -34,21 +34,32 @@ module Devise
         end
 
         def deactivate_session!(session_id)
-          revocable_sessions.where(session_id: session_id).delete_all
+          devise_writer_wrapper do
+            revocable_sessions.where(session_id: session_id).delete_all
+          end
         end
 
         def purge_old_sessions
-          revocable_sessions.order(last_seen_at: :desc).offset(10).destroy_all
+          devise_writer_wrapper do
+            revocable_sessions.order(last_seen_at: :desc).offset(10).destroy_all
+          end
         end
 
         def mark_last_seen!(device_id)
+          devise_writer_wrapper do
+            login_record = revocable_sessions.find_by(device_id: device_id)
+            #skip second to reduce database hit
+            if (Time.now - (login_record.last_seen_at)) >= 60
+              login_record.update_column :last_seen_at, Time.now
+            end
+          end
+        end
+
+        def devise_writer_wrapper
           role = Rails.application.config.active_record.writing_role || :writing
           ActiveRecord::Base.connected_to(role: role) do
               ActiveRecord::Base.connection_handler.while_preventing_writes(false) do
-                login_record = revocable_sessions.find_by(device_id: device_id)
-                #skip second to reduce database hit
-                if (Time.now - (login_record.last_seen_at)) >= 60
-                login_record.update_column :last_seen_at, Time.now
+                yield
               end
             end
           end
